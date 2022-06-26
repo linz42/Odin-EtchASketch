4c3c8171bcb3739f504cb45da6462acdcf906ce6
Lindsay Bruley <muzikismylife13@gmail.com>
2022-06-25 22:34:41 -0700

    Add comments to CSS and Javascript, Started grid code.

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+// Grid creator
+const gridcontainer = document.querySelector('.gridcontainer');
+const square = document.createElement('div');
+const gridvalue = document.querySelector('#gridValue');
+
+console.log(gridvalue);
+
+gridcontainer.append(square);
+
+// Resize squares in grid
+
+
+
+// Slider
+let slider = document.querySelector('#gridSize');
+let output = document.querySelector('#gridValue');
+output.innterHTML = slider.value;
+slider.oninput = function() {
+    output.innerHTML = this.value;
+};
+
+// Reset button
 const reset = document.querySelector('.reset');
 reset.addEventListener('click', function resetpage() {
     location.reload();
